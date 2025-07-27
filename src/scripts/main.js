93034a7ac4faa371ebb74eed3fe3654a5a05984c
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-27 23:08:25 +0300

    clone two row

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const rows = table.querySelectorAll('tr');
+
+rows.forEach((r) => {
+  const cel = r.children;
+
+  const cloneCel = cel[1].cloneNode(true);
+
+  r.insertBefore(cloneCel, cel[cel.length - 1]);
+});
