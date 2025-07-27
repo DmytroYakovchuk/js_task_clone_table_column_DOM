412abaa613cd84e095e84d3fc2a73350bf069b60
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-27 23:30:40 +0300

    add check rows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,10 @@ const rows = table.querySelectorAll('tr');
 rows.forEach((r) => {
   const cel = r.children;
 
+  if (cel.length < 2) {
+    return;
+  }
+
   const cloneCel = cel[1].cloneNode(true);
 
   r.insertBefore(cloneCel, cel[cel.length - 1]);
